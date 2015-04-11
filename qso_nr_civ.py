f6e129b538400348953846d70d7b744ae79b815c
Simeon Bird <spb@ias.edu>
2015-04-11 16:36:03 -0400

    Rework code to generate quasar spectra at varied redshifts

--- qso_nr_civ.py
+++ qso_nr_civ.py
@@ -10,14 +10,15 @@ import hdfsim
 
 class QSONrSpectra(spectra.Spectra):
     """Generate metal line spectra from simulation snapshot"""
-    def __init__(self,num, base, numlos, min_mass, res = 5., cdir = None, savefile="nr_qso_spectra.hdf5", savedir=None, reload_file=True):
+    def __init__(self,num, base, numlos, min_mass, redmin, redmax, res = 5., cdir = None, savefile="nr_qso_spectra.hdf5", savedir=None, reload_file=True):
         #Load halos to push lines through them
         f = hdfsim.get_file(num, base, 0)
         self.OmegaM = f["Header"].attrs["Omega0"]
         self.box = f["Header"].attrs["BoxSize"]
         self.hubble = f["Header"].attrs["HubbleParam"]
         self.atime = f["Header"].attrs["Time"]
-
+        self.redmin = redmin
+        self.redmax = redmax
         self.npart=f["Header"].attrs["NumPart_Total"]+2**32*f["Header"].attrs["NumPart_Total_HighWord"]
         f.close()
         (_, self.sub_mass, cofm, self.sub_radii) = halocat.find_wanted_halos(num, base, min_mass)
@@ -62,10 +63,12 @@ class QSONrSpectra(spectra.Spectra):
     def get_weighted_perp(self, num):
         """Get a random perturbation with a radius weighted by the number of
            quasars in radial bins in the QPQ survey.
-           This needs updating with better lines"""
-        rperp = np.loadtxt("apj503418t2_mrt.txt")
-        rbins = np.arange(np.min(rperp), 1000+np.min(rperp)+1, 100)
-        (hists, rbins) = np.histogram(rperp, rbins)
+        """
+        rperp = np.loadtxt("QSORperpred.txt")
+        #Select quasars in the given redshift range
+        ind = np.where(np.logical_and(rperp[:,1] > self.redmin, rperp[:,0] < self.redmax))
+        rbins = np.arange(30, 1000, 15)
+        (hists, rbins) = np.histogram(rperp[ind,0][0], rbins)
         conv = self.hubble/self.atime
         rbins *= conv
         phi = 2*math.pi*np.random.random_sample(num)
@@ -83,23 +86,23 @@ class QSONrSpectra(spectra.Spectra):
         cofm[:,1]=rr*np.sin(phi)
         return cofm
 
-def do_stuff(snap, path):
+def do_stuff(snap, path, redmin, redmax):
     """Make lines"""
-    halo = QSONrSpectra(snap,path,1000, 12.5)
+    halo = QSONrSpectra(snap,path,1000, 12.5, redmin=redmin, redmax=redmax)
     halo.get_tau("C",4,1548, force_recompute=False)
-    halo.get_tau("H",1,1215, force_recompute=False)
-    halo.get_tau("C",-1,1548, force_recompute=False)
     halo.get_tau("C",2,1334, force_recompute=False)
-    halo.get_tau("Si",2,1526, force_recompute=False)
-    halo.get_tau("Si",4,1393, force_recompute=False)
-    for ion in xrange(1,8):
-        halo.get_density("C",ion)
-    halo.get_density("C",-1)
-    halo.get_velocity("C",4)
+    halo.get_density("C",2)
+    halo.get_density("C",4)
     halo.get_density("H",1)
     halo.save_file()
 
 if __name__ == "__main__":
-   for ss in (1, 4, 7, 9):
-       simbase = myname.get_name(ss, box=25)
-       do_stuff(5, simbase)
+    #For small boxes
+    reds = {5:(1.,2.25), 4:(2.25, 2.75), 3:(2.75, 3.25)}
+    #For big box (with better spacing)
+    #reds = {68:(1.,2.1), 66:(2.1, 2.25), 65:(2.25, 2.4465), 63:(2.4465,2.785), 60:(2.785,5.)}
+    for ss in (7, 9):
+        simbase = myname.get_name(ss, box=25)
+        for (j,zz) in reds.iteritems():
+            do_stuff(j, simbase, zz[0], zz[1])
+
