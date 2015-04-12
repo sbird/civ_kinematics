e93e1dbd3d139a649d41801b8e007792254937d9
Simeon Bird <spb@ias.edu>
2015-04-11 20:27:46 -0400

    Rework CIV plots to work for snapshots loaded at different redshifts

--- civ_plots.py
+++ civ_plots.py
@@ -1,15 +1,54 @@
+# -*- coding: utf-8 -*-
 """Module to plot statistics for the CIV absorbers around DLAs"""
 
 import numpy as np
 import plot_spectra as ps
 import matplotlib.pyplot as plt
 
+def _get_means_binned(spectra, offsets, radial_bins,mean=True):
+    """Get the means of some spectral quantity binned radially"""
+    center = np.array([(radial_bins[i]+radial_bins[i+1])/2. for i in range(0,np.size(radial_bins)-1)])
+    mean_plot_arr = np.zeros(np.size(radial_bins)-1)
+    for ii in np.arange(np.size(radial_bins)-1):
+        arr_bin = spectra[np.where(np.logical_and(offsets > radial_bins[ii], offsets < radial_bins[ii+1]))]
+        if np.size(arr_bin) == 0:
+            continue
+        if mean:
+            mean_plot_arr[ii] = np.mean(arr_bin)
+        else:
+            mean_plot_arr[ii] = np.median(arr_bin)
+    return (center, mean_plot_arr)
+
+def _bootstrap_sample(spectra, offsets, radial_bins, nsamples, error=0):
+    """Generate a Monte Carlo error sample of some spectra."""
+    # Generate some Monte Carlo samples where each element is perturbed by
+    # a Gaussian, sigma given by error.
+    index = np.random.random_integers(0, np.size(spectra)-1, nsamples)
+    bootstrap = spectra[index]
+    bootoffsets = offsets[index]
+    if error > 0.:
+        bootstrap += np.random.normal(0,error,size=nsamples)
+    booted = _get_means_binned(bootstrap, bootoffsets, radial_bins, True)
+    return booted
+
+def _generate_errors(spectra, offsets, radial_bins, nset, nsamples, error=0):
+    """Generate a Monte Carlo error sample of some spectra.
+    Arguments:
+        spectra - set of spectra to subsample
+        radial_bins - set of radial bins to subsample
+        nset - Size of each subsample
+        nsamples - number of subsamples to generate
+        error - If > 0, perturb each element of the subsample with a Gaussian of this sd.
+    """
+    #Generate a bunch of bootstrapped samples, each of size nset,  from our spectra
+    sampled_mean = np.array([_bootstrap_sample(spectra, offsets, radial_bins, nset, error)[1] for _ in xrange(nsamples)])
+    meds = np.median(sampled_mean,axis=0)
+    lerr = meds - np.percentile(sampled_mean, 50-34, axis=0)
+    uerr = np.percentile(sampled_mean, 50+34, axis=0) - meds
+    return [lerr, uerr]
+
 class CIVPlot(ps.PlottingSpectra):
-    """Class to compute various statistics specific to the CIV near DLAs"""
-    def_radial_bins = np.logspace(np.log10(7.5), np.log10(270), 12)
-    obs_bins = np.array([5,100,200,275])
-    nobs = 40
-    color=None
+    """Class to add some methods to PlottingSpectra which are useful for spectra around objects."""
     def get_offsets(self):
         """Get the offsets of each line in proper kpc from its partner"""
         midpoint = self.NumLos/2
@@ -21,64 +60,89 @@ class CIVPlot(ps.PlottingSpectra):
         offsets *= self.atime/self.hubble
         return offsets
 
-    def _bootstrap_sample(self, spectra, offsets, radial_bins, nsamples, error=0):
-        """Generate a Monte Carlo error sample of some spectra."""
-        # Generate some Monte Carlo samples where each element is perturbed by
-        # a Gaussian, sigma given by error.
-        index = np.random.random_integers(0, np.size(spectra)-1, nsamples)
-        bootstrap = spectra[index]
-        bootoffsets = offsets[index]
-        if error > 0.:
-            bootstrap += np.random.normal(0,error,size=nsamples)
-        booted = self._get_means_binned(bootstrap, bootoffsets, radial_bins, True)
-        return booted
+class AggCIVPlot(object):
+    """Class to compute various statistics specific to absorbers around something else, like DLAs or quasars.
+       Aggregates over a varied redshift range.
+       First half of sightlines assumed to go through objects.
+    """
+    def __init__(self,nums, base, redfile, color=None, res=5., savefile="grid_spectra_DLA.hdf5",label="", ls="-", spec_res = 8.):
+        #self.def_radial_bins = np.logspace(np.log10(7.5), np.log10(270), 12)
+        #As is observed
+        self.def_radial_bins = np.array([5,100,200,275])
+        self.color=color
+        self.label=label
+        self.ls=ls
+        #Distribution of redshifts in the data
+        self.datareds = np.loadtxt(redfile)[:,1]
+        self.snaps = [CIVPlot(n, base, res=res, savefile=savefile, spec_res=spec_res) for n in nums]
+        #Set the total number of sightlines as the number from the first snapshot
+        #(ideally all snapshots have the same number), and all snapshots must have the same bin width
+        self.NumLos = self.snaps[0].NumLos
+        self.nbins = self.snaps[0].nbins
+        self.dvbin = self.snaps[0].dvbin
 
-    def _generate_errors(self, spectra, offsets, radial_bins, nset, nsamples, error=0):
-        """Generate a Monte Carlo error sample of some spectra.
-        Arguments:
-            spectra - set of spectra to subsample
-            radial_bins - set of radial bins to subsample
-            nset - Size of each subsample
-            nsamples - number of subsamples to generate
-            error - If > 0, perturb each element of the subsample with a Gaussian of this sd.
-        """
-        #Generate a bunch of bootstrapped samples, each of size nset,  from our spectra
-        sampled_mean = np.array([self._bootstrap_sample(spectra, offsets, radial_bins, nset, error)[1] for _ in xrange(nsamples)])
-        meds = np.median(sampled_mean,axis=0)
-        lerr = meds - np.percentile(sampled_mean, 50-34, axis=0)
-        uerr = np.percentile(sampled_mean, 50+34, axis=0) - meds
-        return [lerr, uerr]
+    def get_tau(self, elem, ion, line):
+        """Get the optical depths by aggregating over the different snapshots according to the correct density distribution"""
+        taus = [qq.get_tau(elem, ion, line) for qq in self.snaps]
+        return self._get_aggregate(taus)
 
-    def _get_means_binned(self,spectra, offsets, radial_bins,mean=True):
-        """Get the means of some spectral quantity binned radially"""
-        center = np.array([(radial_bins[i]+radial_bins[i+1])/2. for i in range(0,np.size(radial_bins)-1)])
-        mean_plot_arr = np.zeros(np.size(radial_bins)-1)
-        for ii in np.arange(np.size(radial_bins)-1):
-            arr_bin = spectra[np.where(np.logical_and(offsets > radial_bins[ii], offsets < radial_bins[ii+1]))]
-            if np.size(arr_bin) == 0:
-                continue
-            if mean:
-                mean_plot_arr[ii] = np.mean(arr_bin)
-            else:
-                mean_plot_arr[ii] = np.median(arr_bin)
-        return (center, mean_plot_arr)
+    def _get_aggregate(self, unaggregated):
+        """Get an aggregated quantity from a quantity passed as a list for the different snapshots
+        according to the correct (observed) redshift distribution"""
+        agg = np.empty_like(unaggregated[0])
+        weights = self.get_redshift_weights()*np.shape(agg)[0]
+        total = 0
+        for jj in xrange(len(unaggregated)-1):
+            agg[total:total+int(weights),:] = unaggregated[jj][total:total+int(weights),:]
+            total+=int(weights)
+        agg[total:,:]= unaggregated[-1][total:,:]
+        return agg
+
+    def get_col_density(self, elem, ion):
+        """Get the optical depths by aggregating over the different snapshots according to the correct density distribution"""
+        cd = [qq.get_col_density(elem, ion) for qq in self.snaps]
+        return self._get_aggregate(cd)
+
+    def get_offsets(self):
+        """Get the optical depths by aggregating over the different snapshots according to the correct density distribution"""
+        off = [qq.get_offsets for qq in self.snaps]
+        agg = np.empty_like(off[0])
+        #Can't just use _get_aggregate because different dimensionality
+        weights = self.get_redshift_weights()*np.shape(off)[0]
+        total = 0
+        for jj in xrange(len(off)-1):
+            agg[total:total+int(weights)] = off[jj][total:total+int(weights)]
+            total+=int(weights)
+        agg[total:]= off[-1][total:]
+        return agg
+
+    def get_redshift_weights(self):
+        """Get the redshift weightings for each snapshot"""
+        reds = [qq.redshift for qq in self.snaps]
+        #Compute the width of redshift bins
+        redbins = [0.,]+ [(reds[i]+reds[i+1])/2. for i in xrange(np.size(reds)-1)]+[5.,]
+        #Compute the weights within each redshift bin
+        weights = np.array([np.size(np.where(np.logical_and(self.datareds > redbins[i], self.datareds < redbins[i+1]))) for i in xrange(np.size(reds))])/1./np.size(self.datareds)
+        return weights
 
     def _plot_radial(self, plot_arr, color, ls, ls2, radial_bins, label=None,mean=True,line=True):
         """Helper function plotting a derived something as a function of radius"""
+        if radial_bins == None:
+            radial_bins = self.def_radial_bins
         center = np.array([(radial_bins[i]+radial_bins[i+1])/2. for i in range(0,np.size(radial_bins)-1)])
         mean_plot_arr = np.zeros(np.size(radial_bins)-1)
         offsets = self.get_offsets()
-        (center, mean_plot_arr) = self._get_means_binned(plot_arr, offsets, radial_bins, mean)
+        (center, mean_plot_arr) = _get_means_binned(plot_arr, offsets, radial_bins, mean)
         if label == None:
             label=self.label
         if line:
             plt.plot(center, mean_plot_arr, color=color, ls=ls, label=label)
         else:
-            yerr = self._generate_errors(plot_arr, offsets, radial_bins, np.size(offsets), 5000)
+            yerr = _generate_errors(plot_arr, offsets, radial_bins, np.size(offsets), 5000)
             plt.errorbar(center, mean_plot_arr, xerr=[radial_bins[:-1]-center,center-radial_bins[1:]],yerr=yerr,fmt='s',color=color, label=label)
         return (center, mean_plot_arr)
 
-    def plot_eq_width_ratio(self, color=None, ls="-", ls2="--", elem="C", ion=4, line=1548, radial_bins = def_radial_bins):
+    def plot_eq_width_ratio(self, color=None, ls="-", ls2="--", elem="C", ion=4, line=1548, radial_bins = None):
         """
         Compute a histogram of the ratios of equivalent widths in pairs of spectra.
 
@@ -92,7 +156,7 @@ class CIVPlot(ps.PlottingSpectra):
         ratio = eq_width[midpoint:]/(eq_width[0:midpoint]+1e-5)
         return self._plot_radial(ratio, color, ls, ls2, radial_bins,mean=False)
 
-    def plot_colden_ratio(self, color=None, ls="-",ls2="--", elem="C", ion=4, elem2=None, ion2=-1,radial_bins = def_radial_bins, label=None):
+    def plot_colden_ratio(self, color=None, ls="-",ls2="--", elem="C", ion=4, elem2=None, ion2=-1,radial_bins = None, label=None):
         """Column density plot; fraction of total in each ion"""
         if elem2 is None:
             elem2 = elem
@@ -101,13 +165,13 @@ class CIVPlot(ps.PlottingSpectra):
         CIV = np.sum(self.get_col_density(elem,ion),axis=1)[midpoint:]
         return self._plot_radial(CIV/(totC+1), color, ls, ls2, radial_bins, label=label,mean=True)
 
-    def plot_colden(self, color=None, ls="-",ls2="--", elem="C", ion=4, radial_bins = def_radial_bins, label=None):
+    def plot_colden(self, color=None, ls="-",ls2="--", elem="C", ion=4, radial_bins = None, label=None):
         """Column density plot"""
         midpoint = self.NumLos/2
         CIV = np.sum(self.get_col_density(elem,ion),axis=1)[midpoint:]
         return self._plot_radial(CIV, color, ls, ls2, radial_bins, label=label,mean=False)
 
-    def plot_covering_fraction(self, eq_thresh = 0.2, color=None, ls="-", ls2 = "--", elem="C", ion=4, line=1548, radial_bins = def_radial_bins, label=None):
+    def plot_covering_fraction(self, eq_thresh = 0.2, color=None, ls="-", ls2 = "--", elem="C", ion=4, line=1548, radial_bins = None, label=None):
         """
         Plot the covering fraction of a given pair line above a threshold in radial bins
         """
@@ -117,10 +181,10 @@ class CIVPlot(ps.PlottingSpectra):
         midpoint = self.NumLos/2
         covering = np.zeros_like(eq_width[midpoint:])
         covering[np.where(eq_width[midpoint:] > eq_thresh)] = 1
-        return self._plot_radial(covering, color, ls, ls2, self.obs_bins, label=label, line=False)
-        #return self._plot_radial(covering, color, ls, ls2, radial_bins, label=label)
+        #return self._plot_radial(covering, color, ls, ls2, self.obs_bins, label=label, line=False)
+        return self._plot_radial(covering, color, ls, ls2, radial_bins, label=label, line=False)
 
-    def plot_covering_fraction_colden(self, cd_thresh = 10**(17.2), color=None, ls="-", elem="H", ion=1, radial_bins = def_radial_bins, label=None):
+    def plot_covering_fraction_colden(self, cd_thresh = 10**(17.2), color=None, ls="-", elem="H", ion=1, radial_bins = None, label=None):
         """
         Plot the covering fraction of a given pair line above a threshold in radial bins
         """
@@ -149,7 +213,7 @@ class CIVPlot(ps.PlottingSpectra):
             total+=this
         return err
 
-    def plot_eq_width(self, color=None, ls="-", ls2 = "--", elem="C", ion=4, line=1548, radial_bins = def_radial_bins, label=None):
+    def plot_eq_width(self, color=None, ls="-", ls2 = "--", elem="C", ion=4, line=1548, radial_bins = None, label=None):
         """
         Plot the equivalent width of a given pair line above a threshold in radial bins
         """
@@ -158,11 +222,11 @@ class CIVPlot(ps.PlottingSpectra):
 #         eq_width = self.equivalent_width(elem, ion, line)+self._get_errors(self.NumLos, elem, ion)
         eq_width = self.equivalent_width(elem, ion, line)
         midpoint = self.NumLos/2  #self.nobs below
-        yerr = self._generate_errors(eq_width[:midpoint], np.zeros(midpoint), np.array([-1,1]), self.NumLos/2.,5000)
+        yerr = _generate_errors(eq_width[:midpoint], np.zeros(midpoint), np.array([-1,1]), self.NumLos/2.,5000)
         plt.errorbar([0,], np.mean(eq_width[:midpoint]), xerr=[[0,],[7.5,]],yerr=yerr, color=color, fmt='s')
-        return self._plot_radial(eq_width[midpoint:], color, ls, ls2, self.obs_bins, label=label,line=False)
+        return self._plot_radial(eq_width[midpoint:], color, ls, ls2, radial_bins, label=label,line=False)
 
-    def plot_flux_vel_offset(self, eq_thresh = 0.2, color=None, ls="-", ls2="--", elem="C", ion=4, line=1548, radial_bins = def_radial_bins):
+    def plot_flux_vel_offset(self, eq_thresh = 0.2, color=None, elem="C", ion=4, line=1548):
         """
         Plot the covering fraction of a given pair line above a threshold in radial bins
         """
@@ -187,30 +251,30 @@ class CIVPlot(ps.PlottingSpectra):
                 vdiff = (vdiff+self.nbins/2)
             offsets.append(vdiff*self.dvbin)
         vel_offsets = np.array(offsets)
-        center = np.array([(radial_bins[i]+radial_bins[i+1])/2. for i in range(0,np.size(radial_bins)-1)])
-        mean_plot_arr = np.zeros(np.size(radial_bins)-1)
-        upper = np.zeros(np.size(radial_bins)-1)
-        lower = np.zeros(np.size(radial_bins)-1)
         offsets = self.get_offsets()[ind]
         lbins = np.arange(0, 300, 20)
         (hist, _) = np.histogram(np.abs(vel_offsets),lbins)
         norm = np.sum(hist)
         plt.bar(lbins[:-1], hist*12./norm, width=20, color=color, label=self.label, alpha=0.4)
         return (lbins, hist)
-#         for ii in np.arange(np.size(radial_bins)-1):
-#             arr_bin = vel_offsets[np.where(np.logical_and(offsets > radial_bins[ii], offsets < radial_bins[ii+1]))]
-#             if np.size(arr_bin) == 0:
-#                 continue
-#             mean_plot_arr[ii] = np.median(arr_bin)
-#             upper[ii] = np.percentile(arr_bin,75)
-#             lower[ii] = np.percentile(arr_bin,25)
-        #plt.plot(center, mean_plot_arr, color=color, ls=ls, label=self.label)
-        #plt.plot(center, lower, color=color, ls=ls2)
-        #plt.plot(center, upper, color=color, ls=ls2)
-#         return (center, mean_plot_arr)
 
     def _get_flux_weigh_vel(self, tau):
         """Compute the flux weighted velocity of a sightline"""
         vel = np.arange(self.nbins)
         mvel = np.sum(vel*tau)/np.sum(tau)
         return mvel
+
+    def equivalent_width(self, elem, ion, line):
+        """Calculate the equivalent width of a line in Angstroms"""
+        tau = self.get_tau(elem, ion, line)
+        #1 bin in wavelength: δλ =  λ . v / c
+        #λ here is the rest wavelength of the line.
+        #speed of light in km /s
+        light = 2.99e5
+        #lambda in Angstroms, dvbin in km/s,
+        #so dl is in Angstrom
+        dl = self.dvbin / light * line
+        eq_width = np.trapz(-np.expm1(-tau),dx=dl, axis=1)
+        #Don't need to divide by 1+z as lambda_X is already rest wavelength
+        return eq_width
+
