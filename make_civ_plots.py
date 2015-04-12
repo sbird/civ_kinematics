416bafaf3beb2bcebcf402171bc72fdbe04f4c63
Simeon Bird <spb@ias.edu>
2015-04-11 20:44:49 -0400

    Turn off WARM winds for quasars; without AGN it will be wildly wrong

--- make_civ_plots.py
+++ make_civ_plots.py
@@ -226,7 +226,7 @@ def do_qso_plots(name, ahalos):
     qso_eq_width("CIV", "C", 4, 1548, name, ahalos)
 
 qsos = []
-for i in (4,7,9):
+for i in (7,9):
     base = myname.get_name(i, box=25)
     qsos.append(ps.AggCIVPlot((3,4,5), base, redfile = "QSORperpred.txt", savefile="nr_qso_spectra.hdf5", color=colors[i], label=labels[i], spec_res = 10.))
     qsos[-1].color=colors[i]
